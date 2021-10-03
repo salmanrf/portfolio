a001c3df66c0a9f5f5075ff054df496f4e5a09c6
salmanrf <salmanrf2@gmail.com>
2021-10-03 09:28:48 +0700

    added skills and new project

--- src/data.js
+++ src/data.js
@@ -1,8 +1,37 @@
 import odinbookPreview from "./assets/img/odin.gif";
 import steamclonePreview from "./assets/img/steamclone.gif";
 import todolistPreview from "./assets/img/todolist.gif";
+import finedealPreview from "./assets/img/finedeal.gif";
 
 export const projectData = [
+  {
+    title: "Fin(d)e Deal",
+    description: "Application for finding game sales and comparing price between stores.",
+    previewImage: finedealPreview,
+    liveLink: "https://finedeal.netlify.app/",
+    sourceLink: "https://github.com/salmanrf/fin-d-e_deal",
+    features: [
+      "Wishlist Highlights",
+      "Highlight sales from steam, gog and humble-bundle",
+      "Paginate sales list from steam, gog and humble-bundle",
+      "Add/remove wishlist",
+      "Authentication",
+      "Compare game prices between stores",
+      "Search games",
+    ],
+    technologies: [
+      "MongoDB",
+      "Express",
+      "React",
+      "Zustand",
+      "Sass",
+      "Redis",
+      "NodeJS",
+      "react-router-dom",
+      "jsonwebtoken",
+      "cheerio",
+    ],
+  },
   {
     title: "OdinBook",
     description: "Fullstack MERN Social Media application",
@@ -25,12 +54,12 @@ export const projectData = [
       "React",
       "react-router-dom",
       "jsonwebtoken",
-      "NodeJS"
-    ]
+      "NodeJS",
+    ],
   },
   {
     title: "Steam Clone (CRUD)",
-    description: "A CRUD application for create, read update and delete-ing games.",
+    description: "A simplified clone of steam.",
     previewImage: steamclonePreview,
     liveLink: "https://steam-crud.herokuapp.com/",
     sourceLink: "https://github.com/salmanrf/steam-clone",
@@ -39,32 +68,17 @@ export const projectData = [
       "Dashboard (Admin)",
       "CRUD games (Admin)",
       "Browse games by genres/developers/publishers",
-      "Create and read forum post"
+      "Create and read forum post",
     ],
-    technologies: [
-      "MongoDB",
-      "Mongoose",
-      "Express",
-      "HTML",
-      "CSS",
-      "Pug",
-      "PassportJS",
-      "NodeJS"
-    ]
+    technologies: ["MongoDB", "Mongoose", "Express", "HTML", "CSS", "Pug", "PassportJS", "NodeJS"],
   },
   {
     title: "Todo List App",
-    description: 
-      `Fullstack MERN application for managing tasks.`
-    ,
+    description: `Fullstack MERN application for managing tasks.`,
     previewImage: todolistPreview,
     liveLink: "https://srf-todo-list.netlify.app",
     sourceLink: "https://github.com/salmanrf/todo-mern/tree/main",
-    features: [
-      "Authentication",
-      "Create Collection",
-      "Create,Read,Update,Delete Todo",
-    ],
+    features: ["Authentication", "Create Collection", "Create,Read,Update,Delete Todo"],
     technologies: [
       "MongoDB",
       "Mongoose",
@@ -72,7 +86,7 @@ export const projectData = [
       "React",
       "jsonwebtoken",
       "react-router-dom",
-      "NodeJS"
-    ]
+      "NodeJS",
+    ],
   },
 ];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from "react";
-import {AiFillGithub} from "react-icons/ai";
-import {FaFacebookSquare} from "react-icons/fa";
-import {HiOutlineMail} from "react-icons/hi";
+import React, { useState, useEffect } from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { FaFacebookSquare } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 import { icons } from "react-icons/lib";
 
 import jsicon from "../assets/img/js.png";
@@ -12,8 +12,11 @@ import mongodbicon from "../assets/img/mongodb.png";
 import expressicon from "../assets/img/express.png";
 import mongooseicon from "../assets/img/mongoose.jpg";
 import nodejsicon from "../assets/img/nodejs.png";
+import redisicon from "../assets/img/redis.png";
+import bootstrapicon from "../assets/img/bootstrap.png";
+import sassicon from "../assets/img/sass.png";
 
-const Home = () => {  
+const Home = () => {
   // Section 1 === main, section 2 === about, section 3 === about
   const [actualSection, setActualSection] = useState(1);
   const [tempSection, setTempSection] = useState(1);
@@ -27,60 +30,54 @@ const Home = () => {
       setActualSection(tempSection);
     }, 250);
   }, [tempSection]);
-  
+
   return (
     <div id="homepage-container">
       {(() => {
-        if(actualSection === 1) 
-          return (
-            <Main section={{current: 1, temp: tempSection, switchTo: setTempSection}} />
-          )
-        
-        if(actualSection === 2)
-            return (
-              <About section={{current: 2, temp: tempSection, switchTo: setTempSection}} />
-            )
+        if (actualSection === 1)
+          return <Main section={{ current: 1, temp: tempSection, switchTo: setTempSection }} />;
 
-        if(actualSection === 3)
-          return (
-            <Skills section={{current: 3, temp: tempSection, switchTo: setTempSection}} />
-          )
+        if (actualSection === 2)
+          return <About section={{ current: 2, temp: tempSection, switchTo: setTempSection }} />;
+
+        if (actualSection === 3)
+          return <Skills section={{ current: 3, temp: tempSection, switchTo: setTempSection }} />;
       })()}
-      <div id="home-navigation" style={{bottom: actualSection !== 1 ? "25px" : "-10%"}}>
-        <div 
+      <div id="home-navigation" style={{ bottom: actualSection !== 1 ? "25px" : "-10%" }}>
+        <div
           className={`home-nav-link ${tempSection === 1 && "home-nav-link-active"}`}
           onClick={() => setTempSection(1)}
         >
           <div className="home-nav-link-bullet"></div>
           <span>Main</span>
         </div>
-        <div 
+        <div
           className={`home-nav-link ${tempSection === 2 && "home-nav-link-active"}`}
           onClick={() => setTempSection(2)}
         >
           <div className="home-nav-link-bullet"></div>
           <span>About</span>
         </div>
-        <div 
+        <div
           className={`home-nav-link ${tempSection === 3 && "home-nav-link-active"}`}
           onClick={() => setTempSection(3)}
         >
           <div className="home-nav-link-bullet"></div>
           <span>Skills</span>
         </div>
-      </div>   
+      </div>
     </div>
   );
-}
+};
 
 const Main = (props) => {
-  const {section} = props;
+  const { section } = props;
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    if(section.current !== section.temp) {
+    if (section.current !== section.temp) {
       setIsMounted(false);
-    } 
+    }
   }, [section.temp]);
 
   useEffect(() => {
@@ -90,28 +87,28 @@ const Main = (props) => {
   }, []);
 
   return (
-    <div 
-      id="home-main" 
+    <div
+      id="home-main"
       style={{
-        transform: isMounted ? "translateY(0%)" : "translateY(25%)", 
-        opacity: isMounted ? "1" : "0"
-        }}
-      >
+        transform: isMounted ? "translateY(0%)" : "translateY(25%)",
+        opacity: isMounted ? "1" : "0",
+      }}
+    >
       <Greetings />
       <Contacts />
-      <HomeLinks switchSection={section.switchTo}/>
+      <HomeLinks switchSection={section.switchTo} />
     </div>
   );
-}
+};
 
 const About = (props) => {
-  const {section} = props;
+  const { section } = props;
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    if(section.current !== section.temp) {
+    if (section.current !== section.temp) {
       setIsMounted(false);
-    } 
+    }
   }, [section.temp]);
 
   useEffect(() => {
@@ -119,9 +116,15 @@ const About = (props) => {
       setIsMounted(true);
     }, 100);
   }, []);
-  
+
   return (
-    <div className="home-section-container" style={{transform: isMounted ? "translateY(0%)" : "translateY(25%)", opacity: isMounted ? "1" : "0"}}>
+    <div
+      className="home-section-container"
+      style={{
+        transform: isMounted ? "translateY(0%)" : "translateY(25%)",
+        opacity: isMounted ? "1" : "0",
+      }}
+    >
       <div className="home-section-header">
         <span>salmanrf@About:~</span>
       </div>
@@ -130,15 +133,15 @@ const About = (props) => {
         <div className="content-section">
           <h3>Background</h3>
           <p>
-            Hi! My name is Salman Rizqi Fatih, I am an aspiring Fullstack Web Developer
-            from Depok, West Java, Indonesia. 
+            Hi! My name is Salman Rizqi Fatih, I am a Fullstack Web Developer from Depok, West Java,
+            Indonesia.
           </p>
         </div>
         <div className="content-section">
           <h3>Education</h3>
           <div className="content-section-item">
             <h4>Computer Engineering And Networking - </h4>
-            <h4>SMKN 1 Cibinong (2014 - 2017)</h4>               
+            <h4>SMKN 1 Cibinong (2014 - 2017)</h4>
           </div>
         </div>
         {/* <div className="content-section">
@@ -150,16 +153,16 @@ const About = (props) => {
       </div>
     </div>
   );
-}
+};
 
 const Skills = (props) => {
-  const {section} = props;
+  const { section } = props;
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    if(section.current !== section.temp) {
+    if (section.current !== section.temp) {
       setIsMounted(false);
-    } 
+    }
   }, [section.temp]);
 
   useEffect(() => {
@@ -167,74 +170,86 @@ const Skills = (props) => {
       setIsMounted(true);
     }, 100);
   }, []);
-  
+
   return (
-      <div 
-        className="home-section-container" 
-        style={{
-          transform: isMounted ? "translateY(0%)" : "translateY(25%)",
-          opacity: isMounted ? "1" : "0"
-        }}
-      >
-        <div className="home-section-header">
-          <span>salmanrf@Skills:~</span>
-        </div>
-        <div className="home-section-content">
-          <h2>Skills</h2>
-          <p>Fullstack Web Development.</p>
-          <div id="techstack">
-            <h2>Technologies i use: </h2>
-            <div id="techstack-container">
-              <div className="stack">
-                <h3>Frontend</h3>
-                <ul>
-                  <li className="stack-item">
-                    <img src={htmlicon} alt="tech-icon" />
-                    <span>HTML</span>
-                  </li>
-                  <li className="stack-item">
-                    <img src={cssicon} alt="tech-icon" />
-                    <span>CSS</span>
-                  </li>
-                  <li className="stack-item">
-                    <img src={jsicon} alt="tech-icon" />
-                    <span>JavaScript</span>
-                  </li>
-                  <li className="stack-item">
-                    <img src={reacticon} alt="tech-icon" />
-                    <span>React</span>
-                  </li>
-                </ul>
-              </div>
-              <div className="stack">
-                <h3>Backend</h3>
-                <ul>
-                  <li className="stack-item">
-                    <img src={mongodbicon} alt="tech-icon" />
-                    <span>MongoDB</span>
-                  </li>
-                  <li className="stack-item">
-                    <img src={expressicon} alt="tech-icon" />
-                    <span>Express</span>
-                  </li>
-                  <li className="stack-item">
-                    <img src={mongooseicon} alt="tech-icon" />
-                    <span>Mongoose</span>
-                  </li>
-                  <li className="stack-item">
-                    <img src={nodejsicon} alt="tech-icon" />
-                    <span>NodeJS</span>
-                  </li>
-                </ul>
-              </div>
+    <div
+      className="home-section-container"
+      style={{
+        transform: isMounted ? "translateY(0%)" : "translateY(25%)",
+        opacity: isMounted ? "1" : "0",
+      }}
+    >
+      <div className="home-section-header">
+        <span>salmanrf@Skills:~</span>
+      </div>
+      <div className="home-section-content">
+        <h2>Skills</h2>
+        <p>Fullstack Web Development.</p>
+        <div id="techstack">
+          <h2>Technologies i use: </h2>
+          <div id="techstack-container">
+            <div className="stack">
+              <h3>Frontend</h3>
+              <ul>
+                <li className="stack-item">
+                  <img src={htmlicon} alt="tech-icon" />
+                  <span>HTML</span>
+                </li>
+                <li className="stack-item">
+                  <img src={cssicon} alt="tech-icon" />
+                  <span>CSS</span>
+                </li>
+                <li className="stack-item">
+                  <img src={bootstrapicon} alt="tech-icon" />
+                  <span>Bootstrap</span>
+                </li>
+                <li className="stack-item">
+                  <img src={sassicon} alt="tech-icon" />
+                  <span>Sass</span>
+                </li>
+                <li className="stack-item">
+                  <img src={jsicon} alt="tech-icon" />
+                  <span>JavaScript</span>
+                </li>
+                <li className="stack-item">
+                  <img src={reacticon} alt="tech-icon" />
+                  <span>React</span>
+                </li>
+              </ul>
+            </div>
+            <div className="stack">
+              <h3>Backend</h3>
+              <ul>
+                <li className="stack-item">
+                  <img src={mongodbicon} alt="tech-icon" />
+                  <span>MongoDB</span>
+                </li>
+                <li className="stack-item">
+                  <img src={expressicon} alt="tech-icon" />
+                  <span>Express</span>
+                </li>
+                <li className="stack-item">
+                  <img src={mongooseicon} alt="tech-icon" />
+                  <span>Mongoose</span>
+                </li>
+                <li className="stack-item">
+                  <img src={nodejsicon} alt="tech-icon" />
+                  <span>NodeJS</span>
+                </li>
+                <li className="stack-item">
+                  <img src={redisicon} alt="tech-icon" />
+                  <span>Redis</span>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
+      </div>
     </div>
   );
-}
+};
 
-// ========= Home Main Sub-Components ========= 
+// ========= Home Main Sub-Components =========
 const Greetings = () => {
   return (
     <div id="greetings">
@@ -251,12 +266,17 @@ const Greetings = () => {
       </div>
     </div>
   );
-}
+};
 
 const Contacts = () => {
   return (
     <div id="contacts">
-      <a href="https://github.com/salmanrf" target="_blank" rel="noreferrer" className="contact-item">
+      <a
+        href="https://github.com/salmanrf"
+        target="_blank"
+        rel="noreferrer"
+        className="contact-item"
+      >
         <AiFillGithub />
         <span>salmanrf</span>
       </a>
@@ -270,7 +290,7 @@ const Contacts = () => {
       </a>
     </div>
   );
-}
+};
 
 const HomeLinks = (props) => {
   return (
@@ -282,10 +302,10 @@ const HomeLinks = (props) => {
       <div className="home-link-item" onClick={() => props.switchSection(3)}>
         <span className="home-link-cursor"></span>
         <span className="home-link-link">Skills</span>
-      </div>  
+      </div>
     </div>
   );
-}
-// ========= Home Main Sub-Components ========= 
+};
+// ========= Home Main Sub-Components =========
 
 export default Home;
